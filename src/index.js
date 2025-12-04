d81ddc44d9e2e10c2b9cd4b8f63c9732d9f6f7bb
Davor Insaurralde <138895434+DavorTW@users.noreply.github.com>
2025-12-04 17:44:25 +0800

    added css loaders and ccs file for styles

--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import "./styles/styles.css";
 import { renderPage } from "./utils/renderPage";
 
 renderPage();
--- src/pages/home.js
+++ src/pages/home.js
@@ -3,6 +3,7 @@ function homePage(){
     
     div.appendChild(restaurantIntro());
     div.appendChild(chefIntro());
+    div.appendChild(theMenu());
 
     return div;
     
@@ -34,5 +35,21 @@ function chefIntro(){
     return(div);
 
 }
+
+function theMenu(){
+    const div = document.createElement("div");
+    const h2 = document.createElement("h2");
+    const p = document.createElement("p");
+
+    h2.textContent = "The menu";
+    p.textContent = "Praesent efficitur elementum urna, a elementum turpis efficitur quis. Cras volutpat orci nec ante interdum tempor non et massa. Vestibulum pharetra quam dui, at sodales massa feugiat sit amet. Suspendisse eget lacus ut velit luctus sodales in et sem. Etiam eu odio tempus est pretium ultrices at a turpis.";
+
+    div.appendChild(h2);
+    div.appendChild(p);
+
+    return div;
+}
+
+
 
 export {homePage}
